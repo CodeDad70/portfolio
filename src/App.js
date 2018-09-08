366487f85c188bd015175b7ee30c37b68fe41c64
CodeDad <github email address>
2018-09-08 14:17:25 -0400

    added react routes to App.js and a components folder

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import './styles/App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          
-          <h1 className="App-title">Eric Strickler Portfolio</h1>
-        </header>
-        <p className="App-intro">
-          Test
-        </p>
+        <Router>
+          <div>
+            <Nav/>
+          </div>
+        </Router>
       </div>
     );
   }
