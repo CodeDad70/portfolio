c0e9be545023cd4c1f6fef7b2568fe0599af12f1
CodeDad <github email address>
2018-09-08 14:31:46 -0400

    created Design.js and Project.js

---
+++ src/components/Design.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Design extends Component {
+  render() {
+    return (
+      
+        <div className='design-stage'>
+
+          
+      </div>
+    )
+  }
+}
+
+export default Design;
---
+++ src/components/Projects.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Projects extends Component {
+  render() {
+    return (
+      
+        <div className='design-stage'>
+
+          
+      </div>
+    )
+  }
+}
+
+export default Projects;
