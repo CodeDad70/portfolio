b415c84f720556f6d530d23181274a1e04bbd53f
codedad70 <eric.strickler@gmail.com>
2018-09-09 16:03:56 -0400

    added a switch statement to the Router in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './styles/App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Nav from './components/Nav'
 import Projects from './components/Projects';
 import Designs from './components/Designs'
@@ -9,12 +10,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
+      <Router>
+        <Switch>
           <div>
-            <Nav/>
+            <Route exact path="/" component={Nav}/>
             <Route exact path="/projects" component={Projects}/>
             <Route exact path="/designs" component={Designs}/>
           </div>
+        </Switch>
         </Router>
       </div>
     );
