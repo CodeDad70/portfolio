0007e55e6e99f4e4adbd3ebf81f70f95ce4bfbde
codedad70 <eric.strickler@gmail.com>
2018-09-13 12:40:03 -0400

    added transistion animation for Designs

--- src/components/Designs.js
+++ src/components/Designs.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 
+import { Transition, config, animated } from 'react-spring'
+import { HashRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom'
 
-class Design extends Component {
-  render() {
-    return (
-      
-        <div className='designs-stage'>
-          <h1>Designs</h1>
+const Designs = ({ style }) => (
+  <animated.div className="mainRoute" style={{ ...style, background: `#ef5350` }}>
+    <div className="mainRouteItem">
+      <p>Designs</p>
+     
+    </div>
+    <Route
+      render={({ location }) => (
+        <div>
+          <Transition
+            native
+            config={config.slow}
+            keys={location.pathname}
+            from={{ transform: 'translateY(500px)', opacity: 0 }}
+            enter={{ transform: 'translateY(0px)', opacity: 1 }}
+            leave={{ transform: 'translateY(500px)', opacity: 0 }}>
+            
+          </Transition>
+        </div>
+      )}
+    />
+  </animated.div>
+)
 
-      </div>
-    )
-  }
-}
 
-export default Design;
+export default Designs;
