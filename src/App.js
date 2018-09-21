453afe090d215a6c5b380adf77825d65a9ca540e
codedad70 <eric.strickler@gmail.com>
2018-09-20 20:59:38 -0400

    added Designslides as a component to the Design page. Removed the link to on Design and removed the route from App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Projects from './components/Projects';
 import Designs from './components/Designs'
 import Contact from './components/Contact'
 import Welcome from './components/Welcome'
-import Designslides from './components/Designslides'
 import './styles/Animation-styles.css'
 
 const App = () => (
@@ -32,7 +31,6 @@ const App = () => (
               leave={{ transform: 'translateY(500px)', opacity: 0 }}>
               {style => (
                 <Switch location={location}>
-                  <Route path="/designslides" component={Designslides} />
                   <Route path="/welcome" render={props => Welcome({ ...props, style })} />
                   <Route path="/projects" render={props => Projects({ ...props, style })} />
                   <Route path="/designs" render={props => Designs({ ...props, style })} />
