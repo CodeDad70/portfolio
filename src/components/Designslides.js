489699e7c136faaf999bb38daf7cb8b1d3bdd242
codedad70 <eric.strickler@gmail.com>
2018-09-20 23:37:12 -0400

    created a test image slide for Designslides

--- src/components/Designslides.js
+++ src/components/Designslides.js
@@ -3,13 +3,20 @@ import { Parallax } from 'react-spring'
 import Icon from '@mdi/react';
 import { mdiRestart } from '@mdi/js';
 import { mdiArrowRightThick } from '@mdi/js';
+import timex from '../images/timexwatch.jpg';
 
-const Page = ({ offset, caption, first, second, gradient, background, onClickForward, onClickBack }) => (
+const Page = ({ offset, caption, first, second, background, imagelink, onClickForward, onClickBack }) => (
   <React.Fragment>
    
     <Parallax.Layer offset={offset} speed={0.2} >
       <div className={`layerBackground ${background}`} />
      
+    </Parallax.Layer>
+
+    <Parallax.Layer offset={offset} speed={0.2} >
+     
+      <img className='image-frame' src={imagelink} /> alt="Timex Layout"/>
+      
     </Parallax.Layer>
 
     <Parallax.Layer className="text headline" horizontal offset={offset} speed={0.9}>
@@ -54,7 +61,7 @@ class Designslides extends React.Component {
       <Parallax className="container" ref="parallax" pages={3} horizontal scrolling={false}>
         <Page offset={0} gradient="pink" background = "red" caption="Test One" first="Huh" second="dolor sit"  onClickForward={() => this.scroll(1)} onClickBack={() => this.scroll(0)} />
         
-        <Page offset={1} gradient="teal" background = "blue" caption="Second Slide" first="consectetur" second="adipiscing elit" onClickForward={() => this.scroll(2)} onClickBack={() => this.scroll(0)} />
+        <Page offset={1} gradient="teal" background = "blue" imagelink={timex} caption="Second Slide" first="consectetur" second="adipiscing elit" onClickForward={() => this.scroll(2)} onClickBack={() => this.scroll(0)} />
 
         <Page offset={2} gradient="tomato" background = "green" caption="Third Slide" first="Morbi quis" second="est dignissim" onClickForward={() => this.scroll(0)} onClickBack={() => this.scroll(1)} />
         
