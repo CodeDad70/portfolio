72484ad9477e6f81f960957f1d06de20bfa942db
codedad70 <eric.strickler@gmail.com>
2018-10-02 15:41:38 -0400

    final slide completed

--- src/components/Designslides.js
+++ src/components/Designslides.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Parallax } from 'react-spring'
 import Icon from '@mdi/react';
 import { mdiRestart } from '@mdi/js';
+import {mdiHome} from '@mdi/js';
 import { mdiArrowRightThick } from '@mdi/js';
 import slideOneA from '../images/Slide-One-A.jpg'
 import slideTwoA from '../images/Slide-Two-A.jpg'
@@ -21,17 +22,14 @@ import Route_Mens_One from '../images/Route_Mens_One.jpg'
 import Route_Mens_Two from '../images/Route_Mens_Two.jpg'
 import Route_Womens from '../images/Route_Womens.jpg'
 import Books from '../images/icons/Books.jpg'
-import Brylane from '../images/icons/Brylane.jpg'
 import Donuts from '../images/icons/Donuts.jpg'
 import Feet from '../images/icons/Feet.jpg'
-import Med from '../images/icons/Med.jpg'
 import Perfume from '../images/icons/Perfume.jpg'
 import Sui_Blue from '../images/icons/Sui_Blue.jpg'
-import Sui_Purple from '../images/icons/Sui_Purple.jpg'
 import Timex from '../images/icons/Timex.jpg'
 
 
-const Page = ({ offset, caption, first, second, background, imagelink, imagelinkB, imagelinkC, imagelinkD, imagelinkE, imagelinkF, imagelinkG, imagelinkH, imagelinkJ, frameOne, frameTwo, frameThree, frameFour, frameFive, frameSix, frameSeven, frameEight, frameNine, onClickForward, onClickBack }) => (
+const Page = ({ offset, caption, first, second, finalCaption, finalCopy, background, imagelink, imagelinkB, imagelinkC, imagelinkD, imagelinkE, imagelinkF, frameOne, frameTwo, frameThree, frameFour, frameFive, frameSix, frameSeven, onClickForward, onClickBack }) => (
   <React.Fragment>
     
     {/*background*/}
@@ -194,6 +192,57 @@ const Page = ({ offset, caption, first, second, background, imagelink, imagelink
       }
     </Parallax.Layer>
 
+    {/*slide ten*/}
+
+    <Parallax.Layer  className = 'slide-wrapper' offset={offset} speed={.5} >
+      {offset===9 &&
+        <div className="container">
+          <div><img className={`image-frame ${frameOne}`} src={imagelink} alt="Layout"/></div>
+        </div>  
+      }
+    </Parallax.Layer>
+
+    <Parallax.Layer  className = 'slide-wrapper' offset={offset} speed={1.25} >
+      {offset===9 &&
+        <div className="container">
+          <div><img className={`image-frame ${frameTwo}`} src={imagelinkB} alt="Layout"/></div>
+        </div>  
+      }
+    </Parallax.Layer>
+
+    <Parallax.Layer  className = 'slide-wrapper' offset={offset} speed={2} >
+      {offset===9 &&
+        <div className="container">
+          <div><img className={`image-frame ${frameThree}`} src={imagelinkC} alt="Layout"/></div>
+        </div>  
+      }
+    </Parallax.Layer>
+
+     <Parallax.Layer  className = 'slide-wrapper' offset={offset} speed={.85} >
+      {offset===9 &&
+        <div className="container">
+          <div><img className={`image-frame ${frameFour}`} src={imagelinkD} alt="Layout"/></div>
+        </div>  
+      }
+    </Parallax.Layer>
+
+     <Parallax.Layer  className = 'slide-wrapper' offset={offset} speed={2.5} >
+      {offset===9 &&
+        <div className="container">
+          <div><img className={`image-frame ${frameFive}`} src={imagelinkE} alt="Layout"/></div>
+        </div>  
+      }
+    </Parallax.Layer>
+
+
+     <Parallax.Layer  className = 'slide-wrapper' offset={offset} speed={3} >
+      {offset===9 &&
+        <div className="container">
+          <div><img className={`image-frame ${frameSix}`} src={imagelinkF} alt="Layout"/></div>
+        </div>  
+      }
+    </Parallax.Layer>
+
 
     {/*copy*/}
 
@@ -203,11 +252,19 @@ const Page = ({ offset, caption, first, second, background, imagelink, imagelink
       </div>
     </Parallax.Layer>
 
-     <Parallax.Layer horizontal offset={offset} speed={0.6}>
+    <Parallax.Layer horizontal offset={offset} speed={0.6}>
 
       <div className='container'>
         <sm className='copy'>{first}</sm>
         <p className='copy'>{second}</p> 
+      </div>
+    </Parallax.Layer>
+
+    <Parallax.Layer horizontal offset={offset} speed={0.9}>
+      <div className='final-caption'>
+        <h3 >{finalCaption}</h3>
+        <p>{finalCopy}</p>
+        
       </div>
     </Parallax.Layer>
 
@@ -216,22 +273,26 @@ const Page = ({ offset, caption, first, second, background, imagelink, imagelink
 
      <Parallax.Layer offset={offset} speed={0.4} >
       
-      {offset!==0 &&  
+      {offset!==0  &&  
            
           <Icon path={mdiArrowRightThick} className = "arrow-back" size={1.5} rotate={90} color='black' onClick={onClickBack}/>
 
       }
+
+      
+
       {offset!==9 &&
           
           <Icon path={mdiArrowRightThick}  className = "arrow-forward" size={1.5} color='black' onClick={onClickForward}/>
         
       }
       {offset===9 &&
-        
-          <Icon path={mdiRestart} size={1.5} className = "restart" flip-h color='black' onClick={onClickForward}/>
-    
-      }
+        <div>
 
+          <Icon path={mdiRestart} size={1.5} className = "restart" flip-h color='black' onClick={onClickForward}/>
+          
+        </div>  
+      }
       </Parallax.Layer>
    
       
@@ -262,7 +323,7 @@ class Designslides extends React.Component {
 
          <Page offset={8} background = "slide-one" frameFive = "frame-five" frameSix = "frame-six" frameSeven = "frame-seven" imagelink={Route_Womens} imagelinkB={Route_Mens_One} imagelinkC={Route_Mens_Two} caption="Client: Route 66" first="Design of pocket tags, size stickers and hang tags for jeans"  onClickForward={() => this.scroll(9)} onClickBack={() => this.scroll(7)} />
 
-         <Page offset={9} background = "slide-one" frameOne="frame-fourteen" frameTwo = "frame-fifteen" frameThree = "frame-sixteen" frameFour = "frame-seventeen" frameFive = "frame-eighteen" frameSix = "frame-nineteen" frameSeven = "frame-twenty" frameEight = "frame-twentyone" frameNine = "frame-twentytwo" imagelink={Books} imagelinkB={Donuts} imagelinkC={Timex} imagelinkD={Feet} imagelinkE={Brylane} imagelinkF={Sui)Blue} imagelinkG={Perfume} imagelinkH={Sui_Purple} imagelinkJ={Med} onClickForward={() => this.scroll(0)} onClickBack={() => this.scroll(8)} /> 
+         <Page offset={9} background = "slide-one" frameOne="frame-fourteen" frameTwo = "frame-fifteen" frameThree = "frame-sixteen" frameFour = "frame-seventeen" frameFive = "frame-eighteen" frameSix = "frame-nineteen" imagelink={Books} imagelinkB={Donuts} imagelinkC={Timex} imagelinkD={Feet} imagelinkE={Perfume} imagelinkF={Sui_Blue} finalCaption="Clients Include  " finalCopy="Timex, Williams-Sonoma, Marvel, Etc. Etc." onClickForward={() => this.scroll(0)} onClickBack={() => this.scroll(8)} /> 
 
       </Parallax>
     )
