a8e7bb4071fda01401e74c9857477cc1bfd65927
codedad70 <eric.strickler@gmail.com>
2018-10-12 16:13:42 -0400

    formatted project slide one and fixed urls for both slides

--- src/components/Projectslides.js
+++ src/components/Projectslides.js
@@ -67,10 +67,14 @@ const Page = ({ offset, caption, first, second, specs_link, test_url, specs_one,
           <Icon path={mdiRestart} size={1.5} className = "restart" flip-h color='black' onClick={onClickForward}/>
         }
 
-        
-         <div className="specs-link"> <a href={specs_link} target="_blank">Visit this site</a></div>
-         
+        {offset ===0 &&
+         <div className="specs-link-one"> <a href={specs_link} target="_blank">Visit this site</a></div>
+        }
 
+        {offset !==0 &&
+         <div className="specs-link"> <a href={specs_link} target="_blank">Visit this site</a></div>
+        }
+          
        </Parallax.Layer>
 
      
@@ -87,10 +91,10 @@ class Projectslides extends React.Component {
       <Parallax  ref="parallax" pages={3} horizontal scrolling={false}>
         
         {/*slide one*/}
-          <Page offset={0} background = "slide-one" frame = "p-frame-one" imagelink={sproutHq} caption="Project: SproutHQ" first="Create picture cards to track your childs vital stats and milestones." specs="Test copy goes here" onClickForward={() => this.scroll(1)} onClickBack={() => this.scroll(0)} />
+          <Page offset={0} frame = "p-frame-one" imagelink={sproutHq} caption="Project: SproutHQ" first="Create picture cards to track your childs vital stats and milestones." specs_one= "• This app uses a Rails API in the backend with JavaScript and jQuery in the front. " specs_two = "• Designed the database schema and configured Active Record associations" specs_three= "• Utilized jQuery and JavaScript to render a dynamic front end that allows users to view cards and generate milestones without a page refresh" specs_four="• Used Devise for password secutiry, user authentication and Paperclip for image uploads" specs_link="https://floating-atoll-18673.herokuapp.com/" onClickForward={() => this.scroll(1)} onClickBack={() => this.scroll(0)} />
         
         {/*slide two*/}
-          <Page offset={1} background = "slide-one" frame = "p-frame-one" imagelink={filmFinder} caption="Project: FilmFinder" first="Search for your favorite movies or actors and create a watch list." specs_link="https://github.com/CodeDad70/portfolio/commits/master" specs_one= "• Utilizes Rails to generate an API in the back end for database persistence " specs_two = "• Utilizes React with Redux and Thunk for centralized storage of state and asynchronous requests to both The Movie Database API and the Rails API." specs_three= "• Utilizes react-routes to create a restful route structure but still employs the speed of the virtual DOM, single page app, and client side rendering" specs_four="• Utilized React with Redux and Thunk for centralized storage of state and asynchronous requests to The Movie DB API and the Rails API"  onClickForward={() => this.scroll(2)} onClickBack={() => this.scroll(0)} />
+          <Page offset={1} frame = "p-frame-one" imagelink={filmFinder} caption="Project: FilmFinder" first="Search for your favorite movies or actors and create a watch list." specs_link="https://agile-river-92406.herokuapp.com/" specs_one= "• Utilizes Rails to generate an API in the back end for database persistence " specs_two = "• Utilizes React with Redux and Thunk for centralized storage of state and asynchronous requests to both The Movie Database API and the Rails API." specs_three= "• Utilizes react-routes to create a restful route structure but still employs the speed of the virtual DOM, single page app, and client side rendering" specs_four="• Utilized React with Redux and Thunk for centralized storage of state and asynchronous requests to The Movie DB API and the Rails API"  onClickForward={() => this.scroll(2)} onClickBack={() => this.scroll(0)} />
         
         {/*slide three*/}
           <Page offset={2} background = "slide-one" frame = "p-frame-one" imagelink={portfolio} caption="Project: ES Design" first="This app is built with React and utilizes React-spring for transistion animations"  specs="Test copy goes here" onClickForward={() => this.scroll(0)} onClickBack={() => this.scroll(1)} />
