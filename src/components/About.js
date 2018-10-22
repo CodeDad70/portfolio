981611c7e080139867fe4e81bfc3dca0a9be6a79
codedad70 <eric.strickler@gmail.com>
2018-10-21 23:46:34 -0400

    copy fixes to projects page

--- src/components/About.js
+++ src/components/About.js
@@ -12,9 +12,9 @@ const Contact = ({ style }) => (
     
     <img className="about-image" src={About_Pic}/>
     <div className="icon-container">  
-    <a href = "google.com" target = "blank" > <img className = "email" src={Email} /> </a>
-    <a href = "google.com" target = "blank" ><img  className="github" src={GitHub} /></a>
-    <a href = "google.com" target = "blank" ><img className="linkedin" src={LinkedIn} /></a>
+    <a href = "mailto:eric.strickler@gmail.com" > <img className = "email" src={Email} /> </a>
+    <a href = "https://github.com/CodeDad70?tab=repositories" target = "blank" ><img  className="github" src={GitHub} /></a>
+    <a href = "https://www.linkedin.com/in/eric-strickler/" target = "blank" ><img className="linkedin" src={LinkedIn} /></a>
     </div>
     <p className="about-text">Eric Strickler is a full stack web developer and graphic designer with a passion for bringing ideas from conception to reality. Creativity has been the driving force throughout my career in graphic design and working with brands like Williams-Sonoma, Timex, Yoga Journal, and Tovolo, as well as in my personal life, whether I’m playing guitar or making a painting. I first grew interested in web development when, as a graphic designer, I wanted to learn what was under the hood of the websites I was helping to design. I bring strong skills in problem solving and a keen eye for design and detail that’s an ideal hybrid for helping companies achieve their vision for original, vibrant web apps. My lifelong passion for creativity has informed every project I undertake, and I’m intrinsically motivated to learn new concepts and techniques to utilize that passion.</p>
     
--- src/components/Projectslides.js
+++ src/components/Projectslides.js
@@ -74,11 +74,15 @@ const Page = ({ offset, caption, first, second, specs_link, test_url, specs_one,
         
         }
 
-        {offset !==0 &&
+        {offset ===1 &&
        
          <div className="specs-link"> <a href={specs_link} target="_blank">Visit this site</a></div>
          
         }
+
+       
+
+
           
        </Parallax.Layer>
 
@@ -96,13 +100,13 @@ class Projectslides extends React.Component {
       <Parallax  ref="parallax" pages={3} horizontal scrolling={false}>
         
         {/*slide one*/}
-          <Page offset={0} frame = "p-frame-one" imagelink={sproutHq} caption="Project: SproutHQ" first="Create picture cards to track your childs vital stats and milestones." specs_one= "• This app uses a Rails API in the backend with JavaScript and jQuery in the front. " specs_two = "• Designed the database schema and configured Active Record associations" specs_three= "• Utilized jQuery and JavaScript to render a dynamic front end that allows users to view cards and generate milestones without a page refresh" specs_four="• Used Devise for password secutiry, user authentication and Paperclip for image uploads" specs_link="https://floating-atoll-18673.herokuapp.com/" onClickForward={() => this.scroll(1)} onClickBack={() => this.scroll(0)} />
+          <Page offset={0} frame = "p-frame-one" imagelink={sproutHq} caption="Project: SproutHQ" first="Create picture cards to track your childs vital stats and milestones." specs_one= "• This app has a Rails API in the backend with JavaScript and jQuery in the front. " specs_two = "• The database schema uses  Active Record associations" specs_three= "• Utilizes jQuery and JavaScript to render a dynamic front end that allows users to view cards and generate milestones without a page refresh" specs_four="• Employs Devise for password secutiry, user authentication and Paperclip for image uploads" specs_link="https://floating-atoll-18673.herokuapp.com/" onClickForward={() => this.scroll(1)} onClickBack={() => this.scroll(0)} />
         
         {/*slide two*/}
-          <Page offset={1} frame = "p-frame-one" imagelink={filmFinder} caption="Project: FilmFinder" first="Search for your favorite movies or actors and create a watch list." specs_link="https://agile-river-92406.herokuapp.com/" specs_one= "• Utilizes Rails to generate an API in the back end for database persistence " specs_two = "• Utilizes React with Redux and Thunk for centralized storage of state and asynchronous requests to both The Movie Database API and the Rails API." specs_three= "• Utilizes react-routes to create a restful route structure but still employs the speed of the virtual DOM, single page app, and client side rendering" specs_four="• Utilized React with Redux and Thunk for centralized storage of state and asynchronous requests to The Movie DB API and the Rails API"  onClickForward={() => this.scroll(2)} onClickBack={() => this.scroll(0)} />
+          <Page offset={1} frame = "p-frame-one" imagelink={filmFinder} caption="Project: FilmFinder" first="Search for your favorite movies or actors and create a watch list." specs_link="https://agile-river-92406.herokuapp.com/" specs_one= "• Utilizes Rails to generate an API in the back end for database persistence " specs_two = "• Uses React with Redux and Thunk for centralized storage of state and asynchronous requests to both The Movie Database API and the Rails API." specs_three= "• Has react-routes to create a REST-ful route structure but still employs the speed of the virtual DOM, single page app, and client side rendering" specs_four="• Employs React with Redux and Thunk for centralized storage of state and asynchronous requests to The Movie DB API and the Rails API"  onClickForward={() => this.scroll(2)} onClickBack={() => this.scroll(0)} />
         
         {/*slide three*/}
-          <Page offset={2} background = "slide-one" frame = "p-frame-one" imagelink={portfolio} caption="Project: ES Design" first="This app is built with React and utilizes React-spring for transistion animations"  specs="Test copy goes here" onClickForward={() => this.scroll(0)} onClickBack={() => this.scroll(1)} />
+          <Page offset={2} background = "slide-one" frame = "p-frame-one" imagelink={portfolio} caption="Project: ES Design" first="This app is built with React to utitlize the virtual DOM for a single page app"  specs_two = "• Uses React springs, a library of JavaScript primitives, for transisition animations." specs_three= "• Has react-routes to create a restful route structure but still employs the speed of the virtual DOM, single page app, and client side rendering" specs_four="• Employs React routes for REST-ful route structure while still utilizing the single page app and the virtual DOM "  onClickForward={() => this.scroll(0)} onClickBack={() => this.scroll(1)} />
 
       </Parallax>
       
